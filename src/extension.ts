122cf6a9ce69c299c497d7c5e2743fb0eebada29
simon <simon.chen@gamesofa.com>
2017-12-15 18:11:45 +0800

    add: util.ts

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,10 @@ import * as fs from 'fs';
 import { CreatorCompletionItemProvider } from './CreatorCompletionItemProvider'
 import { CreatorDeclaration } from './creatorDeclaration'
 import { CreatorWatch } from './creatorWatch'
+import { isCocosCreatorProjectFolder } from './util';
 
 export function activate(ctx: ExtensionContext) {
-
-    try {
-        var cocosProjectJson = path.resolve(workspace.rootPath, 'project.json');
-        var json = JSON.parse(fs.readFileSync(cocosProjectJson, 'utf8'));
-    } catch (e) { }
-
-    const JS_MODE: DocumentFilter = {
-        language: 'javascript',
-        scheme: 'file',
-    };
-
-    if (json && json.engine === 'cocos2d-html5') {
-        //ctx.subscriptions.push(languages.registerCompletionItemProvider(JS_MODE, new CreatorCompletionItemProvider(), '.'));
-        //ctx.subscriptions.push(languages.registerDefinitionProvider(JS_MODE, new CreatorDeclaration()));
+    if (isCocosCreatorProjectFolder()) {
         ctx.subscriptions.push(new CreatorWatch());
     }
 }
--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,44 @@
 // https://github.com/Microsoft/vscode-go/blob/master/src/util.ts
 import * as vscode from 'vscode'
 
+import * as path from 'path';
+import * as fs from 'fs';
+import { isString } from 'util';
+
+export function isCocosCreatorProjectFolder(): boolean {
+	if (!vscode.workspace.workspaceFolders) {
+		return false;
+	}
+
+	var rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+	if (!fs.existsSync(path.resolve(rootPath, 'assets'))) {
+		return false;
+	}
+
+	try {
+		var jsonPath = path.resolve(rootPath, 'project.json');
+		var json = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
+	} catch (e) { }
+
+	return json && json.engine === 'cocos2d-html5';
+}
+
+export function isCocosCreatorProjectFile(document: vscode.TextDocument | string): boolean {
+	if (!vscode.workspace.workspaceFolders) {
+		return false;
+	}
+
+	var fsPath = isString(document) ? document : document.uri.fsPath;
+	if (path.extname(fsPath).toLowerCase() !== '.js') {
+		return false;
+	}
+
+	var rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+	var relative = path.relative(rootPath, fsPath);
+	var inAssets = relative.split(path.sep)[0].toLocaleLowerCase() == 'assets';
+	return inAssets;
+}
+
 export function isPositionInString(document: vscode.TextDocument, position: vscode.Position): boolean {
 	let lineText = document.lineAt(position.line).text;
 	let lineTillCurrentPosition = lineText.substr(0, position.character);
